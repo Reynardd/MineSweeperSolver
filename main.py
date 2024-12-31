4b3abea7cbf470044ab742adb686fe4b5ed3048e
Reynard <reynardipg@gmail.com>
2024-12-31 03:32:43 +0330

    main handler was added

---
+++ main.py
@@ -0,0 +1,13 @@
+from Solver import MineSweeperSolver
+from interface import MineSweeperInterface
+import time
+interface = MineSweeperInterface()
+solver = MineSweeperSolver()
+while interface.get_game_state()=='Playing':
+    solver.update_knowledge(interface.parse_elements())
+    moves = solver.generate_next_moves()
+    for move in moves:
+        action,x,y =move[0],move[1],move[2]
+        if action in ('S','R'):
+            interface.click_on_cell(x,y)
+input()
